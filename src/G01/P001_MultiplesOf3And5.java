ebc40d38596b0e45c57b534119449b590fd8509c
haroldalmon <haroldemail@gmail.com>
2015-04-01 17:56:37 -0500

    test

---
+++ src/G01/P001_MultiplesOf3And5.java
@@ -0,0 +1,45 @@
+package G01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Strategy: Instead of doing a modulus check on every
+ * number, sum all the numbers by adding 3 and then
+ * adding 5.
+ * This solution might be faster than checking every
+ * number with division by 3 and 5 but I have not tested the
+ * difference.
+ * @author halmon
+ */
+public class P001_MultiplesOf3And5 {
+	public static void main(String[] args) {
+		System.out.printf("result = %d\n", new P001_MultiplesOf3And5().run(10));
+	}
+	long run(int upperLimit) {
+		long result = 0;
+		
+		int multOf3 = 3;
+		int multOf5 = 5;
+
+		while (multOf3 < upperLimit ) {
+			result += multOf3;
+			multOf3 += 3;
+		}
+		
+		// Exclude numbers that are a multiple of 3 because they are already in the total... 
+		while (multOf5 < upperLimit ) {
+			if( multOf5 % 3 != 0) {
+				result += multOf5;
+			}
+			multOf5 += 5;
+		}	
+		
+		return result;
+	}
+	@Test
+	public void test1() {
+		assertTrue( run(1000) == 233168 );
+	}
+}
---
+++ src/G01/P002_EvenFibonacciNumbers.java
@@ -0,0 +1,37 @@
+package G01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+/**
+ * Strategy: Use a for-loop since we do not know the term
+ * of the Fibonacci number we re seeking. 
+ * @author Harold
+ *
+ */
+public class P002_EvenFibonacciNumbers {
+	public static void main(String[] args) {
+		System.out.printf("Result: %d", new P002_EvenFibonacciNumbers().run());
+
+	}
+	long run() {
+		long result = 2;
+		int num1 = 1;
+		int num2 = 2;
+		int sum = 3;
+		while (sum < 4000000) {
+			if( sum % 2 == 0) {
+				result += sum;				
+			}
+			num1 = num2;
+			num2 = sum;
+			sum = num1 + num2;
+		}
+		
+		return result;
+	}
+	@Test
+	public void test1() {
+		assertTrue( run() == 4613732 );
+	}
+}

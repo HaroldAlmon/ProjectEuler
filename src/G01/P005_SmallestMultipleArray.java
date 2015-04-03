dd49eaa48c680e6b1897434a26cb5818a8a528e2
haroldalmon <haroldemail@gmail.com>
2015-04-02 20:17:30 -0500

    Initial checkin.

---
+++ src/G01/P005_SmallestMultipleArray.java
@@ -0,0 +1,77 @@
+package G01;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class P005_SmallestMultipleArray {
+	public static void main(String[] args) {
+		new P005_SmallestMultipleArray().run(20);
+	}
+
+	long run(int maxDivisor) {
+		P003_LargestPrimeFactor maxFactor = new P003_LargestPrimeFactor();
+		
+		// factorCount the *count* of the factor N in position int[N] for a number.
+		// If 2*3*3 are factors then factorCount[2] = 1, factorCount[2] = 3;
+		
+		// This would be faster with a HashMap.
+		int[] factorCount = new int[20];
+		int[] finalCount = new int[20];
+		int[] factors;
+		
+		// Get all of the prime factors of all the numbers from 2 thru N
+		// Note that the prime factors will repeat like 2*2*3.
+		for(int divisor=2; divisor<=maxDivisor; divisor += 1) {
+			factors = maxFactor.getFactors(divisor);
+			
+			// Count the identical factors in the current number...
+			for(int factIdx = 0; factIdx < factors.length; factIdx++) {
+				factorCount[factors[factIdx]] += 1;
+			}
+			
+			// Copy the factor count to the final count if we have a bigger total.
+			// This causes some numbers to drop out of the final product. e.g. 4
+			// is not include because 12 = 2 * 2 * 3 that includes 4 as a sub-product.
+			// So if the final product includes 2 * 2 and (* 3) then that number is
+			// divisible by both 4 and 12.
+			for(int factIdx = 0; factIdx < maxDivisor; factIdx++) {
+				if(factorCount[factIdx] > finalCount[factIdx]) {
+					finalCount[factIdx] = factorCount[factIdx];
+				}
+			}
+			Arrays.fill(factorCount, 0);
+			
+			System.out.printf("Factors of %d: %s\n", divisor, Arrays.toString(factors));
+		};
+		
+		long result = 1;
+
+		// Take the factors and multiply them together to get the result...
+		for(int factIdx = 0; factIdx < maxDivisor; factIdx++) {
+			if(finalCount[factIdx] != 0 ) {
+				System.out.printf("Factor %d count %d\n", factIdx, finalCount[factIdx]);
+				result *= (long) Math.pow(factIdx, finalCount[factIdx]);
+			}
+		}
+		System.out.println("Total = " + result);
+		return result;
+	}
+	
+	@Test
+	public void test1() {
+		assertTrue( run(20) == 232792560 );
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue( run(10) == 2520);
+	}
+	
+	@Test
+	public void test3() {
+		assertTrue( run(15) == 360360);
+	}
+}
---
+++ src/G01/P005_SmallestMultipleHashMap.java
@@ -0,0 +1,89 @@
+package G01;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import org.junit.Test;
+public class P005_SmallestMultipleHashMap {
+	public static void main(String[] args) {
+		new P005_SmallestMultipleHashMap().run(20);
+	}
+
+	long run(int maxDivisor) {
+		P003_LargestPrimeFactor maxFactor = new P003_LargestPrimeFactor();
+		
+		// Holds the *count* of the factor n in position int[n] for a number.
+		// If 2*2*2 are factors then factorCount[2] = 3;
+		
+		HashMap<Integer, Integer> factorCount = new HashMap<>();
+
+		// Use a TreeMap instead of a HashMap so I can retrieve the keys in a sorted order.
+		// Each entry contains the factor and the number of times the factor appears.
+		// 2*3*3=18 would be represented by (2,1), (3,2).
+		TreeMap<Integer, Integer> finalCount = new TreeMap<>();
+		int[] factors;
+
+		for(int divisor=2; divisor<=maxDivisor; divisor += 1) {
+			factors = maxFactor.getFactors(divisor);
+			
+			// Count the identical factors in the current number...
+			for(int factIdx = 0; factIdx < factors.length; factIdx++) {
+				if (factorCount.containsKey( factors[factIdx] )) {
+					
+					// Increment the factor count.  There is hidden unboxing and boxing here.
+					factorCount.put(factors[factIdx], factorCount.get(factors[factIdx]) + 1);
+				} else {
+					
+					
+					// A new factor, set the count to 1...
+					factorCount.put(factors[factIdx], 1);
+				};
+			}
+			
+			// Copy the factor count to the final count if we have a bigger total...
+			for(Integer factor : factorCount.keySet()) {
+				if(finalCount.containsKey(factor)) {
+
+					// Only copy the total if the total exceeds the existing factor count...
+					if(factorCount.get(factor) > finalCount.get(factor)) {
+						finalCount.put(factor, factorCount.get(factor));
+					}
+				} else {
+					// If the factor is not in the final count then copy the total into the final count.
+					finalCount.put(factor, factorCount.get(factor));
+				}
+			}
+			factorCount.clear();
+			
+			System.out.printf("Factors of %d: %s\n", divisor, Arrays.toString(factors));
+		};
+		
+		long result = 1;
+
+		// Take the factors out of the finalCount map and multiply them together to get the result...
+		for (Integer factor : finalCount.keySet()) {
+			System.out.printf("Factor %d count=%d\n", factor, finalCount.get(factor));
+			result *= (long) Math.pow(factor, finalCount.get(factor));
+		}
+		System.out.println("Total = " + result);
+		return result;
+	}
+	
+	@Test
+	public void test1() {
+		assertTrue( run(20) == 232792560 );
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue( run(10) == 2520);
+	}
+	
+	@Test
+	public void test3() {
+		assertTrue( run(15) == 360360);
+	}
+}

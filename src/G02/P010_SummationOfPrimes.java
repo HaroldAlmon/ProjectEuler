5047790877dea32bcee7d6c38b3ee1baf5faba5a
Harold Almon <haroldemail@gmail.com>
2015-04-14 18:48:41 -0500

    Refactor JUnit.

--- src/G02/P010_SummationOfPrimes.java
+++ src/G02/P010_SummationOfPrimes.java
@@ -1,19 +1,13 @@
 package G02;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static G01.Formatter.errorText;
 
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 public class P010_SummationOfPrimes {
 	static int testNo = 1;
-
-	public static void main(String[] args) {
-		P010_SummationOfPrimes main = new P010_SummationOfPrimes();
-		System.out.print( "prime=" + main.run(20));
-	}
-	
 	private long run(int limit) {
 		int candidate;
 		int primeCount;
@@ -24,7 +18,7 @@ public class P010_SummationOfPrimes {
 		primeCount = 1;
 		candidate = 3;
 		
-		System.out.printf("limit = %d\n", limit);
+		//System.out.printf("limit = %d\n", limit);
 
 		while (candidate < limit) {
 
@@ -47,6 +41,7 @@ public class P010_SummationOfPrimes {
 			}
 			candidate += 2;
 		}
+		System.out.printf("Result(%d) = %d\n", limit, primeSum);
 		return primeSum;
 	}
 
@@ -55,29 +50,22 @@ public class P010_SummationOfPrimes {
 		System.out.printf("Running unit test...%d\n", testNo);
 		testNo += 1;
 	}
-	//@Test
-	public void test1() {
-		assertTrue( run(2_000_000) == 104_743 );
-	}
 	
-	@Test
-	public void test10() {
-		// takes 133 seconds on an i7 laptop
-		System.out.printf("Sum = %d\n", run(2_000_000));
-	}
-	
-	//@Test
-	public void test2() {
-		assertTrue( run(6) == 13 );
-	}
+	@Test(timeout = 300_000)
+	@Ignore
+	public void TwoMillionPrimes() {
+		// takes ~133 seconds on an i7 laptop
+		assertEquals( errorText("Incorrect sum"), 142913828922L, run(2_000_000) );
 
-	@BeforeClass
-	public static void beforeClass() {
-		System.out.println("Starting unit test.");
 	}
 	
-	@AfterClass
-	public static void afterClass() {
-		System.out.println("Unit test completed.");
+	@Test(timeout = 500)
+	public void SixPrimes() {
+		assertEquals( errorText("Incorrect sum"), run(6), 10 );
+	}
+	
+	@Test(timeout = 500)
+	public void TenPrimes() {
+		assertEquals( errorText("Incorrect sum"), run(10), 17 );
 	}
 }

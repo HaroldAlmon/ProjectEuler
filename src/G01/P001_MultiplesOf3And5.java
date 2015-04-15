022dd8ebd735dc2f96c5dbe7c3595769af2cdd55
Harold Almon <haroldemail@gmail.com>
2015-04-14 19:39:03 -0500

    Refactor to eliminate comment.

--- src/G01/P001_MultiplesOf3And5.java
+++ src/G01/P001_MultiplesOf3And5.java
@@ -1,48 +1,46 @@
 package G01;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
  * Strategy: Brute force.
- * Instead of doing a modulus check on every
- * number, sum all the numbers by adding 3 and then
- * adding 5.
+ * 
  * This solution might be slower or faster than checking every
  * number with division by 3 and 5 but I have not tested the
  * difference.
  * @author Harold Almon
  */
 public class P001_MultiplesOf3And5 {
-	public static void main(String[] args) {
-		System.out.printf("result = %d\n", new P001_MultiplesOf3And5().run(10));
-	}
-	long run(int upperLimit) {
-		long result = 0;
+	int run(int upperLimit) {
+		int result = 0;
 		
-		int multOf3 = 3;
-		int multOf5 = 5;
+		int multipleOf3 = 3;
+		int multipleOf5 = 5;
 
-		while (multOf3 < upperLimit ) {
-			result += multOf3;
-			multOf3 += 3;
+		while (multipleOf3 < upperLimit ) {
+			result += multipleOf3;
+			multipleOf3 += 3;
 		}
 		
-		// Exclude numbers that are a multiple of 3 because they are already in the total... 
-		while (multOf5 < upperLimit ) {
-			if( multOf5 % 3 != 0) {
-				result += multOf5;
-			}
-			multOf5 += 5;
-		}	
-		
+		while (multipleOf5 < upperLimit ) {
+			result = addToResultIfNotMultipleOfThree(result, multipleOf5);
+			multipleOf5 += 5;
+		}
+		return result;
+	}
+
+	private int addToResultIfNotMultipleOfThree(int result, int multOf5) {
+		if( multOf5 % 3 != 0) {
+			result += multOf5;
+		}
 		return result;
 	}
 
 	@Test(timeout=100)
 	public void CountMultiplesOfThreeAndFive() {
-		assertEquals( "Incorrect mutiples count, ", 233168, run(1000) );
+		int result = run(1000);
+		System.out.printf("%s\n", result);
+		assertEquals( "Incorrect mutiples count, ", 233168, result );
 	}
 }

3465b8dfb64e0722f1666a10eb03da8d71953a06
haroldalmon <haroldemail@gmail.com>
2015-04-16 18:21:26 -0500

    Reactor solution.

--- src/G02/P010_SummationOfPrimes.java
+++ src/G02/P010_SummationOfPrimes.java
@@ -8,7 +8,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 public class P010_SummationOfPrimes {
 	static int testNo = 1;
-	private long run(int limit) {
+	private long sumOfPrimesLessThan(int candidateLimit) {
 		int candidate;
 		int primeCount;
 		long primeSum = 2;
@@ -20,11 +20,12 @@ public class P010_SummationOfPrimes {
 		
 		//System.out.printf("limit = %d\n", limit);
 
-		while (candidate < limit) {
+		while (candidate < candidateLimit) {
 
 			boolean isPrime = true;		
 			for(int i = 0; i < primeCount; i++) {
-				if ( candidate % primes[i] == 0 ) {
+				//if ( candidate % primes[i] == 0 ) {
+				if ( isRemainderOfDivisionZero(candidate, primes[i]) ) {
 					isPrime = false;
 					break;
 				}
@@ -39,10 +40,17 @@ public class P010_SummationOfPrimes {
 				}
 				primeCount += 1;
 			}
-			candidate += 2;
+			candidate = nextCandidate(candidate);
 		}
-		System.out.printf("Result(%d) = %d\n", limit, primeSum);
+		System.out.printf("Result(%d) = %d, no. of primes %d\n", candidateLimit, primeSum, primeCount);
 		return primeSum;
+	}
+
+	private int nextCandidate(int candidate) {
+		return candidate + 2;
+	}
+	private boolean isRemainderOfDivisionZero (int numerator, long denominator) {
+		return numerator % denominator == 0;
 	}
 
 	@Before
@@ -52,20 +60,19 @@ public class P010_SummationOfPrimes {
 	}
 	
 	@Test(timeout = 300_000)
-	@Ignore
 	public void TwoMillionPrimes() {
 		// takes ~133 seconds on an i7 laptop
-		assertEquals( errorText("Incorrect sum"), 142913828922L, run(2_000_000) );
+		assertEquals( errorText("Incorrect sum"), 142913828922L, sumOfPrimesLessThan(2_000_000) );
 
 	}
 	
 	@Test(timeout = 500)
 	public void SixPrimes() {
-		assertEquals( errorText("Incorrect sum"), run(6), 10 );
+		assertEquals( errorText("Incorrect sum"), sumOfPrimesLessThan(6), 10 );
 	}
 	
 	@Test(timeout = 500)
 	public void TenPrimes() {
-		assertEquals( errorText("Incorrect sum"), run(10), 17 );
+		assertEquals( errorText("Incorrect sum"), sumOfPrimesLessThan(10), 17 );
 	}
 }

05174b8cddc5cb8b1502098bca9da77e4747208d
haroldalmon <haroldemail@gmail.com>
2015-04-19 05:16:43 -0500

    Refactor.

--- src/G02/P017_NumberLetterCounts.java
+++ src/G02/P017_NumberLetterCounts.java
@@ -8,35 +8,26 @@ import org.junit.Test;
 public class P017_NumberLetterCounts {
 	int Land = "and".length();
 	
-	static int L1 = "one".length();
-	static int L2 = "two".length();
-	static int L3 = "three".length();
-	static int L4 = "four".length();
-	static int L5 = "five".length();
-	static int L6 = "six".length();
-	static int L7 = "seven".length();
-	static int L8 = "eight".length();
-	static int L9 = "nine".length();
-	
+	@SuppressWarnings("serial")
 	static List<Integer> digitLen = new ArrayList<Integer>() {
-		private static final long serialVersionUID = -8110120484993544128L;
-
-		{ 
-			add(L1);
-			add(L2);
-			add(L3);
-			add(L4);
-			add(L5);
-			add(L6);
-			add(L7);
-			add(L8);
-			add(L9);
-		}
+		{
+			// TODO: USe Arrays.asList() instead
+			add("one".length());
+			add("two".length());
+			add("three".length());
+			add("four".length());
+			add("five".length());
+			add("six".length());
+			add("seven".length());
+			add("eight".length());
+			add("nine".length());
+		};
 	};
 	
 	@SuppressWarnings("serial")
 	static List<Integer> tens = new ArrayList<Integer>() {
 		{ 
+			// TODO: USe Arrays.asList() instead
 			add("twenty".length());
 			add("thirty".length());
 			add("forty".length());
@@ -47,23 +38,27 @@ public class P017_NumberLetterCounts {
 			add("ninety".length());
 		}
 	};
-	
-	int L1to9 = L1+L2+L3+L4+L5+L6+L7+L8+L9;
-	
+
+	int L1to9 = sum1to9(digitLen);
+
+	private int sum1to9(List<Integer> digitLen) {
+		List<Integer> d = digitLen;
+		return d.get(0)+d.get(1)+d.get(2)+d.get(3)+d.get(4)+d.get(5)+d.get(6)+d.get(7)+d.get(8);
+	}
+
 	int L10to19= 
-			"ten".length() +
-			"eleven".length() +
-			"twelve".length() +
-			"thirteen".length() +
-			"fourteen".length() +
-			"fifteen".length() +
-			"sixteen".length() +
-			"seventeen".length() +
-			"eighteen".length() +
-			"nineteen".length();
+		"ten".length() +
+		"eleven".length() +
+		"twelve".length() +
+		"thirteen".length() +
+		"fourteen".length() +
+		"fifteen".length() +
+		"sixteen".length() +
+		"seventeen".length() +
+		"eighteen".length() +
+		"nineteen".length();
 	
 	int L1to19 = L1to9 + L10to19;
-	int L20to99 = 0;
 	int L1to99 = 0;
 	
 	int Lhundred = "hundred".length();
@@ -71,26 +66,41 @@ public class P017_NumberLetterCounts {
 	
 	int Land1to99 = 0;
 	
-	public int getCount() {
-		int result;
-		// 20-99
-		for( int i : P017_NumberLetterCounts.tens)
-			L20to99 += i * 10 + L1to9;
-		L1to99 = L1to19 + L20to99;
+	public int numberLetterCounts() {
+		int result = 0;
+
+		L1to99 = sum1To99();
+		Land1to99 = sumAnd1To99();
+
 		result = L1to99;
-		Land1to99 = L1to99 + Land * 99;
-		
-		// 100 - 999
-		System.out.printf("AND 1 to 99 = %d%n", Land1to99);
-		for( int i : P017_NumberLetterCounts.digitLen)
-			result += (i + Lhundred) * 100 + Land1to99;
-		
-		// 1000
-		result += L1 + Lthousand;
+		result += sum100to999(result);
+		result += lengthOf1000(result);
 		return result;
 	}
+	private int sumAnd1To99() {
+		return L1to99 + Land * 99;
+	}
+	private int sum1To99() {
+		return L1to19 + sum20to99();
+	}
+	private int lengthOf1000(int result) {
+		result += digitLen.get(0) + Lthousand;
+		return result;
+	}
+	private int sum100to999(int result) {
+		for( int i : digitLen)
+			result += (i + Lhundred) * 100 + Land1to99;
+		return result;
+	}
+	private int sum20to99() {
+		int L20to99 = 0;
+		for( int i : P017_NumberLetterCounts.tens)
+			L20to99 += i * 10 + L1to9;
+		return L20to99;
+	}
+
 	@Test
 	public void test1() {
-		System.out.printf("Count = %d", getCount());
+		System.out.printf("Count = %d", numberLetterCounts());
 	}
 }

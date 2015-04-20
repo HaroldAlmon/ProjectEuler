4c21533e112c62f7b470ac6098b3b849076164c5
haroldalmon <haroldemail@gmail.com>
2015-04-20 06:11:58 -0500

    Initial version.

---
+++ src/G01/FuseTester.java
@@ -0,0 +1,12 @@
+package G01;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class FuseTester {
+	@Test
+	public void overloader() {
+		fail("You have a blown use! Success!");
+	}
+}

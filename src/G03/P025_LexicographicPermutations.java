47395e733faf16f521a65b2ccfa0db728934d789
Harold Almon <haroldemail@gmail.com>
2015-04-24 20:37:59 -0500

    Initial version.

---
+++ src/G03/P025_LexicographicPermutations.java
@@ -0,0 +1,36 @@
+package G03;
+import javax.lang.model.type.NoType;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class P025_LexicographicPermutations {
+
+	private String lexicographicPermutations(int noOfDigits) {
+		int[] digits = new int[noOfDigits];
+		for(int i = 0; i < noOfDigits; i++) {
+			digits[i] = i;
+		}
+			// while p > 0
+			// 		set p to 2nd last char
+			//		while p >= 0
+			//		if smaller digit on the right of p
+			//			swap p and smallest digit to the right of p
+			// 			sort digits to the right of p
+			//			print digits
+			//			break;
+			//		else
+			//			p = p - 1;
+			//		end while
+			//			
+			// end if
+			// end while
+		return "";
+	}
+	@Test
+	public void Permutation() {
+		String permutation;
+		permutation = lexicographicPermutations(3); 
+		assertEquals("Permutation is not correct", "123", permutation);
+	}
+}

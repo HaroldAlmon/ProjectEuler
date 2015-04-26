14784b3a1b851d28a44aaadf80542d4914934406
haroldalmon <haroldemail@gmail.com>
2015-04-26 02:19:25 -0500

    Refactor.

--- src/G03/P020_FactorialDigitSum.java
+++ src/G03/P020_FactorialDigitSum.java
@@ -1,33 +1,33 @@
 package G03;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import java.math.BigInteger;
-
 import org.junit.Test;
 
 public class P020_FactorialDigitSum {
-	public int getTotal(int n) {
+	public int factorialDigitSum(int upperLimit) {
+		String factorial;
+		int sumOfDigits = 0;
+
 		BigInteger product = new BigInteger("1");
 		BigInteger multiplier = new BigInteger("2");
 		BigInteger one = new BigInteger("1");
 		
-		for (int i = 2; i <= n; i++) {
+		for (int i = 2; i <= upperLimit; i++) {
 			product = product.multiply(multiplier);
 			multiplier = multiplier.add(one);
 		}
-		
-		int sum = 0;
-		String factorial = product.toString();
-		for (int i = 0; i < factorial.length(); i++) {
-			sum += Character.getNumericValue(factorial.charAt(i));
+
+		factorial = product.toString();
+		for (int position = 0; position < factorial.length(); position++) {
+			sumOfDigits += Character.getNumericValue(factorial.charAt(position));
 		}
-		return sum;
+		return sumOfDigits;
 	}
 	@Test(timeout = 500)
-	public void test1() {
-		int result = getTotal(100);
-		System.out.printf("sum = %d%n", result);
-		assertTrue(648 == result);
+	public void FactorialDigitSum() {
+		int sumOfDigits = factorialDigitSum(100);
+		System.out.printf("factorialDigitSum(100) = %d%n", sumOfDigits);
+		assertEquals(sumOfDigits, 648 );
 	}
 }

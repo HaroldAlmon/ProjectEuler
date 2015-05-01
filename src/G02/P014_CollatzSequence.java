86661fb5707551a3d22f36822016747e98295708
haroldalmon <haroldemail@gmail.com>
2015-05-01 17:02:47 -0500

    Refactor

--- src/G02/P014_CollatzSequence.java
+++ src/G02/P014_CollatzSequence.java
@@ -1,31 +1,45 @@
 package G02;
-
+/**
+ * Strategy: Simple Mathematics
+ * @author Harold Almon
+ *
+ */
 import org.junit.Test;
 
 public class P014_CollatzSequence {
-	public long numberProducingLongestChain(int upperLimit) {
-		long result = 0;
-		long longest = 0;
-		int length;
-		for (long num = 2; num < upperLimit - 1; num++) {
-			length = 1;
-			long n = num;
-			while (n != 1) {
-				if (n % 2 == 0)
-					n /= 2;
-				else
-					n = 3 * n + 1;
-				length += 1;
-			}
-			if (length > longest) {
-				longest = length;
-				result = num;
+	public long collatzSequence(int upperLimit) {
+		long numberProducingLongestChain = 0;
+
+		numberProducingLongestChain = enumerateCandidates(upperLimit,numberProducingLongestChain);
+		return numberProducingLongestChain;
+	}
+	private long enumerateCandidates(int upperLimit, long numberProducingLongestChain) {
+		for (long candidate = 2; candidate < upperLimit - 1; candidate++) {
+			long longestSequence = 0;
+			int sequenceLength = 1;
+			sequenceLength = calculateSequenceLength(sequenceLength, candidate);
+			if (sequenceLength > longestSequence) {
+				longestSequence = sequenceLength;
+				numberProducingLongestChain = candidate;
 			}
 		}
-		return result;
+		return numberProducingLongestChain;
 	}
+	private int calculateSequenceLength(int length, long candidate) {
+		// n is an alias for candidate...
+		long n = candidate;
+		while (n != 1) {
+			if (n % 2 == 0)
+				n /= 2;
+			else
+				n = 3 * n + 1;
+			length += 1;
+		};
+		return length;
+	}
+
 	@Test(timeout = 20_000)
 	public void NumberProducingLongestChain() {
-		System.out.printf("numberProducingLongestChain(1_000_000) = %d%n",numberProducingLongestChain(1_000_000));
+		System.out.printf("numberProducingLongestChain(1_000_000) = %d%n",collatzSequence(1_000_000));
 	}
 }

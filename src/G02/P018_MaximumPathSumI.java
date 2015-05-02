2f80ad32a261d1736c6bd8dedbaa305f47e4253c
haroldalmon <haroldemail@gmail.com>
2015-05-02 09:02:46 -0500

    Move comment.

--- src/G02/P018_MaximumPathSumI.java
+++ src/G02/P018_MaximumPathSumI.java
@@ -1,40 +1,46 @@
 package G02;
-
+/**
+ * @author Harold Almon
+ * @param pyramid - An array containing integers shaped like a pyramid.
+ * @return The maximum total from top to bottom of a triangle array.
+ */
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class P018_MaximumPathSumI {
-	public int maximumPathSum() {
-		int[][] pyramid = { 
-				{75}, 
-				{95,64}, 
-				{17,47,82}, 
-				{18,35,87,10}, 
-				{20,04,82,47,65}, 
-				{19,01,23,75,03,34}, 
-				{88,02,77,73,07,63,67}, 
-				{99,65,04,28,06,16,70,92}, 
-				{41,41,26,56,83,40,80,70,33}, 
-				{41,48,72,33,47,32,37,16,94,29}, 
-				{53,71,44,65,25,43,91,52,97,51,14}, 
-				{70,11,33,28,77,73,17,78,39,68,17,57}, 
-				{91,71,52,38,17,14,91,43,58,50,27,29,48}, 
-				{63,66,04,68,89,53,67,30,73,16,69,87,40,31}, 
-				{04,62,98,27,23, 9,70,98,73,93,38,53,60,04,23} 
-				}; 
-
-		
+	public int maximumPathSumI(int[][] pyramid) {
 		for (int row = pyramid.length - 2; row >= 0; row--) {
 			for(int col = 0; col < pyramid[row].length; col++ ) {
-				pyramid[row][col] += Math.max( pyramid[row+1][col], pyramid[row+1][col+1] );
+				int rowBelowLeftColumnValue = pyramid[row+1][col];
+				int rowBelowRightColumnValue = pyramid[row+1][col+1];
+				pyramid[row][col] += Math.max( rowBelowLeftColumnValue, rowBelowRightColumnValue );
 			}
 		}
 		return pyramid[0][0];
 	}
-	@Test
+
+	@Test(timeout = 50)
 	public void MaximumPathSum() {
-		int result = maximumPathSum();
-		System.out.printf("maximumPathSum() = %d%n", result);
-		assertEquals(1074, result);
+		int maximumSum = maximumPathSumI(pyramid);
+		System.out.printf("maximumPathSum() = %d%n", maximumSum);
+		assertEquals(1074, maximumSum);
 	}
+
+	private int[][] pyramid = { 
+		{75}, 
+		{95,64}, 
+		{17,47,82}, 
+		{18,35,87,10}, 
+		{20,04,82,47,65}, 
+		{19,01,23,75,03,34}, 
+		{88,02,77,73,07,63,67}, 
+		{99,65,04,28,06,16,70,92}, 
+		{41,41,26,56,83,40,80,70,33}, 
+		{41,48,72,33,47,32,37,16,94,29}, 
+		{53,71,44,65,25,43,91,52,97,51,14}, 
+		{70,11,33,28,77,73,17,78,39,68,17,57}, 
+		{91,71,52,38,17,14,91,43,58,50,27,29,48}, 
+		{63,66,04,68,89,53,67,30,73,16,69,87,40,31}, 
+		{04,62,98,27,23, 9,70,98,73,93,38,53,60,04,23} 
+	}; 
 }

27670fc369fa9d54fa44c4ca72edf15152cbc3b3
haroldalmon <haroldemail@gmail.com>
2015-05-03 15:05:21 -0500

    Faster solution.

--- src/G01/P001_MultiplesOf3And5.java
+++ src/G01/P001_MultiplesOf3And5.java
@@ -1,44 +1,31 @@
 package G01;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 /** Strategy: Brute force. Simple mathematics. */
 public class P001_MultiplesOf3And5 {
 	public int multiplesOf3And5(int upperLimit) {
-		int sumOfMultiples = 0;
-
-		sumOfMultiples = addMultiplesof3(upperLimit, sumOfMultiples);
-		sumOfMultiples = addMultiplesOf5(upperLimit, sumOfMultiples);
+		int sumOfMultiples = 0;		
+		
+		sumOfMultiples += calculateSumBelowLimit(upperLimit, 3);
+		sumOfMultiples += calculateSumBelowLimit(upperLimit, 5);
+		sumOfMultiples -= calculateSumBelowLimit(upperLimit, 15);
 		return sumOfMultiples;
 	}
 	
-	private int addMultiplesof3(int upperLimit, int sumOfMultiples) {
-		int multipleOf3 = 3;
-		while (multipleOf3 < upperLimit ) {
-			sumOfMultiples += multipleOf3;
-			multipleOf3 += 3;
-		}
-		return sumOfMultiples;
+	private int calculateSumBelowLimit(int upperLimit, int divisor) {
+		int result = 0;
+		int adjustedLimit = (upperLimit-1)/divisor;
+
+		// This is a variation of the sum(1..n) formula.
+		result = adjustedLimit * (adjustedLimit + 1) / 2 * divisor ;
+		return result;
 	}
 
-	private int addMultiplesOf5(int upperLimit, int sumOfMultiples) {
-		int multipleOf5 = 5;		
-		while (multipleOf5 < upperLimit ) {
-			sumOfMultiples = addToSumIfNotMultipleOfThree(sumOfMultiples, multipleOf5);
-			multipleOf5 += 5;
-		}
-		return sumOfMultiples;
-	}
-
-	private int addToSumIfNotMultipleOfThree(int sumOfMultiples, int multOf5) {
-		if( multOf5 % 3 != 0) {
-			sumOfMultiples += multOf5;
-		}
-		return sumOfMultiples;
-	}
-
-	@Test(timeout=100)
+	@Test
 	public void SumOfMultiplesOf3And5Below1000() {
 		int sumOfMultiples = multiplesOf3And5(1000);
 		System.out.printf("multiplesOf3And5(1000) = %s%n", sumOfMultiples);

dfa1c7fffdab50eb49d40dcf3aa9453a19a1117c
haroldalmon <haroldemail@gmail.com>
2015-05-07 06:20:54 -0500

    Rename function.

--- src/G01/P002_EvenFibonacciNumbers.java
+++ src/G01/P002_EvenFibonacciNumbers.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 /** Strategy: Simple mathematics. */
 public class P002_EvenFibonacciNumbers {
-	int sumOfEvenFibonacciNumbers(int upperLimit) {
-		int sumOfEvenFibonacciNumbers = 2;
+	int EvenFiboNumbersSum(int upperLimit) {
+		int EvenFiboNumbersSum = 2;
 		int fibo1 = 1;
 		int fibo2 = 2;
 		int nextFibo = 3;
@@ -14,13 +14,13 @@ public class P002_EvenFibonacciNumbers {
 		 * of the Fibonacci number we are seeking. */
 		while (nextFibo < upperLimit) {
 			if( isEven(nextFibo) ) {
-				sumOfEvenFibonacciNumbers += nextFibo;				
+				EvenFiboNumbersSum += nextFibo;				
 			}
 			fibo1 = fibo2;
 			fibo2 = nextFibo;
 			nextFibo = fibo1 + fibo2;
 		}
-		return sumOfEvenFibonacciNumbers;
+		return EvenFiboNumbersSum;
 	}
 	
 	private boolean isEven(int n) {
@@ -29,8 +29,8 @@ public class P002_EvenFibonacciNumbers {
 
 	@Test(timeout=500)
 	public void SumOfEvenTermsLessThanFourMillion() {
-		int result = sumOfEvenFibonacciNumbers(4_000_000);
-		System.out.printf("sumOfEvenFibonacciNumbersBelow(4_000_000)=%s%n", result);
-		assertEquals( "Incorrect sum of even Fibonacci numbers that do not exceed 4 million", 4613732, result );
+		int result = EvenFiboNumbersSum(4_000_000);
+		System.out.printf("EvenFiboNumbersSum(4_000_000)=%s%n", result);
+		assertEquals( "Incorrect sum of even Fibo numbers that do not exceed 4 million", 4613732, result );
 	}
 }

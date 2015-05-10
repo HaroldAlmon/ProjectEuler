ea77dc0947be7e472fb802985d685a055b206c1a
haroldalmon <haroldemail@gmail.com>
2015-05-10 07:13:52 -0500

    Refactor

--- src/G01/P001_MultiplesOf3And5.java
+++ src/G01/P001_MultiplesOf3And5.java
@@ -3,6 +3,8 @@ package G01;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import common.FastTest;
 
@@ -17,31 +19,31 @@ public class P001_MultiplesOf3And5 {
 		MultiplesSum -= calculateSumBelowLimit(upperLimit, 15);
 		return MultiplesSum;
 	}
-	
+
+	// This is a variation of the sum(1..n) formula.
+	//
+	// Sum(1...kn) = k * n * (n + 1)
+	//                   -----------
+	//                        2
+	// The formula includes n, the problem does not.
 	private int calculateSumBelowLimit(int upperLimit, int divisor) {
 		int result = 0;
-		// This is a variation of the sum(1..n) formula.
-		//
-		// Sum(1...kn) = k * n * (n + 1)
-		//                   -----------
-		//                        2
-		// Also the formula includes n, the problem does not.
 		int n = (upperLimit-1)/divisor;
 		result = divisor * n * (n + 1) / 2  ;
 		return result;
 	}
 
-	@Test
+	@Test(timeout=50)
 	public void SumOfMultiplesOf3And5Below1000() {
 		int sumOfMultiples = multiplesOf3And5(1_000);
 		System.out.printf("multiplesOf3And5(1000) = %s%n", sumOfMultiples);
-		assertEquals( "Incorrect sum of multiples of 3 or 5 below 1,000", 233168, sumOfMultiples );
+		assertThat( "Incorrect sum of multiples of 3 or 5 below 1,000", sumOfMultiples, is(233168) );
 	}
 	
 	@Test(timeout=100)
 	public void SumOfMultiplesOf3And5Below10_000() {
 		int sumOfMultiples = multiplesOf3And5(10_000);
 		System.out.printf("multiplesOf3And5(10_000) = %s%n", sumOfMultiples);
-		assertEquals( "Incorrect sum of multiples of 3 or 5 below 10,000", 23331668, sumOfMultiples );
+		assertThat( "Incorrect sum of multiples of 3 or 5 below 10,000", sumOfMultiples, is(23331668) );
 	}
 }

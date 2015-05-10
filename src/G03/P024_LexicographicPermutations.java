52b7be68a47273123fe67fb635025b8053502ee9
haroldalmon <haroldemail@gmail.com>
2015-05-10 06:44:44 -0500

    Daily checkin.

--- src/G03/P024_LexicographicPermutations.java
+++ src/G03/P024_LexicographicPermutations.java
@@ -1,4 +1,6 @@
 package G03;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,25 +8,50 @@ public class P024_LexicographicPermutations {
 
 	private String lexicographicPermutations(int noOfDigits) {
 		int[] digits = new int[noOfDigits];
-		for(int i = 0; i < noOfDigits; i++) {
+		for (int i = 0; i < noOfDigits; i++) {
 			digits[i] = i;
 		}
-			// while p > 0
-			// 		set p to 2nd last char
-			//		while p >= 0
-			//		if smaller digit on the right of p
-			//			swap p and smallest digit to the right of p
-			// 			sort digits to the right of p
-			//			print digits
-			//			break;
-			//		else
-			//			p = p - 1;
-			//		end while
-			//			
-			// end if
-			// end while
+		System.out.printf("%s%n", Arrays.toString(digits));
+		int k = noOfDigits - 2;
+		int p = noOfDigits - 2;
+		while (k > 0) {
+			while (p >= 0) {
+				int temp = 0;
+				// if smaller digit on the right of p
+				smallestPosition = smallestPosition(digits, p);
+				if (digits[p] < ) {
+					// swap p and smallest digit to the right of p
+					temp = digits[p + 1];
+					digits[p + 1] = digits[p];
+					digits[p] = temp;
+					// sort digits to the right of p
+					// print all digits
+					System.out.printf("String = %s%n", Arrays.toString(digits));
+					p = noOfDigits - 2;
+					break;
+				} else {
+					p = p - 1;
+				}
+			}
+			k -= 1;
+		}
 		return "";
 	}
+	
+	private int smallestPosition(int[] digits, int p) {
+		int min = Integer.MAX_VALUE;
+		for (int i = p+2; i < digits.length; i++) {
+			if(digits[i+1] < min) {
+				min = digits[i+1];
+			}
+		}
+		return min;
+	}
+	
+	private int smallestDigit(int[] digits, int startPosition) {
+		return 0;
+	}
+	// 012
 	@Test
 	public void Permutation() {
 		String permutation;

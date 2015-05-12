fced8f5c56b9df2bb7e6d91da3a9e049abb566a7
haroldalmon <haroldemail@gmail.com>
2015-05-12 06:29:10 -0500

    Refactor names.

--- src/G03/P027_QuadraticPrimes.java
+++ src/G03/P027_QuadraticPrimes.java
@@ -1,48 +1,52 @@
 package G03;
 
 import static org.junit.Assert.assertEquals;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Test;
-/**
- * Strategy: Brute force.
- * @author Harold Almon
- *
- */
+/** Strategy: Brute force. */
 public class P027_QuadraticPrimes {
 	static Set<Integer> primes;
+	private final boolean isDebug = false;
 
-	public int getProduct(int limit) {
+	public int quadraticPrimes(int limit) {
 		primes = getPrimes(168);
 		
-		int sum;
-		int max_a = 0;
-		int max_b = 0;
-		int numOfPrimes;
+		int maxCoefficientA = 0;
+		int maxCoefficientB = 0;
 		int maxNumOfPrimes = 0;
+
 		for (int a = -limit - 1; a < limit; a++) {
 			for (int b = -limit + 1; b < limit; b++) {
+				int numOfPrimes;
 				numOfPrimes = 0;
-
-				for (int n = 0; n < limit; n++) {
-					sum = n*n + a*n + b;
-					if ( isPrime(sum) )
-						numOfPrimes += 1;
-					else
-						break;
-				}
+				numOfPrimes = calculatePrimesForAandB(limit, a, b);
 				if (numOfPrimes > maxNumOfPrimes)  {
 					maxNumOfPrimes = numOfPrimes;
-					max_a = a;
-					max_b = b;
+					maxCoefficientA = a;
+					maxCoefficientB = b;
 				}
 			}
 		}
-		System.out.printf("max no. of primes = %d, a=%d, b=%d\n", maxNumOfPrimes, max_a, max_b);
-		return max_a * max_b;
+
+		if(isDebug) {
+			System.out.printf("max no. of primes = %d, a=%d, b=%d%n", maxNumOfPrimes, maxCoefficientA, maxCoefficientB);
+		}
+		return maxCoefficientA * maxCoefficientB;
+	}
+	private int calculatePrimesForAandB(int limit, int a, int b) {
+		int sum;
+		int numOfPrimes = 0;
+		for (int n = 0; n < limit; n++) {
+			sum = n*n + a*n + b;
+			if ( isPrime(sum) )
+				numOfPrimes += 1;
+			else
+				break;
+		}
+		return numOfPrimes;
 	}
 	private boolean isPrime(int n) {
 		return primes.contains(n);
@@ -52,40 +56,42 @@ public class P027_QuadraticPrimes {
 	// wraps the prime numbers in Integer objects
 	// and then returns a Set.
 	private Set<Integer> getPrimes(int limit) {
-		int candidate;
+		int primeCandidate;
 		int primeCount;
 		
 		Integer primes[] = new Integer[limit];
 		
 		primes[0] = 2;
 		primeCount = 1;
-		candidate = 3;
+		primeCandidate = 3;
 
 		while (primeCount < limit) {
 			boolean isPrime = true;
 			// Check the candidate to see if it is divisible by 
 			// any of the known primes calculated so far.
 			for(int i = 0; i < primeCount; i++) {
-				if ( candidate % primes[i] == 0 ) {
+				if ( primeCandidate % primes[i] == 0 ) {
 					isPrime = false;
 					break;
 				}
 			}
 
 			if (isPrime == true) {
-				primes[primeCount] = candidate;
+				primes[primeCount] = primeCandidate;
 				primeCount += 1;
 			}
-			candidate += 2;
+			primeCandidate += 2;
 		}
 		// Primes must be Integer wrapped for the HashSet constructor.
 		return new HashSet<Integer>( Arrays.asList(primes) );
 	}
-	@Test
-	public void test1() {
-		int result = 0;
-		result = getProduct(1000);
-		System.out.printf("Result=%s\n", result);
-		assertEquals( result, -59231);
+
+	@Test(timeout = 2_000)
+	public void QuadraticPrimes() {
+		int productOfCoeffiecients = 0;
+
+		productOfCoeffiecients = quadraticPrimes(1_000);
+		System.out.printf("quadraticPrimes(1_000) = %s%n", productOfCoeffiecients);
+		assertEquals( productOfCoeffiecients, -59231);
 	}
 }

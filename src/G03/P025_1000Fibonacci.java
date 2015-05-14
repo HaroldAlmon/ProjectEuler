8f0d73931f06e38813935bc7ce3a9e45e6d98fee
haroldalmon <haroldemail@gmail.com>
2015-05-14 07:36:50 -0500

    Refactor

--- src/G03/P025_1000Fibonacci.java
+++ src/G03/P025_1000Fibonacci.java
@@ -1,13 +1,14 @@
 package G03;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigInteger;
 
 import org.junit.Test;
 
 public class P025_1000Fibonacci {
-	public int getTerm(int limit) {
-		final boolean FALSE = false;
-		int termNo = 2;
+	public int getIndex(int limit) {
+		final boolean isDebug = false;
+		int index = 2;
 		BigInteger temp;
 		BigInteger prev = BigInteger.ONE;
 		BigInteger sum = BigInteger.ONE;
@@ -16,15 +17,17 @@ public class P025_1000Fibonacci {
 			prev = sum;
 			sum = sum.add(temp);
 			
-			termNo += 1;
-			if(FALSE)
+			index += 1;
+			if(isDebug)
 				System.out.printf("sum = %d%n", sum);
 		}
-		return termNo;
+		return index;
 	}
 	
-	@Test
+	@Test(timeout = 500)
 	public void test() {
-		System.out.printf("term=%d%n",getTerm(1000));
+		int index = getIndex(1000);
+		System.out.printf("getIndex(1000) = %d%n", index);
+		assertEquals(4782, index);
 	}
 }

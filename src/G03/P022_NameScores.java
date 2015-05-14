4db09d74e3d4b9c5a679e9dc2a371d371169fffd
haroldalmon <haroldemail@gmail.com>
2015-05-14 07:41:22 -0500

    Rename function

--- src/G03/P022_NameScores.java
+++ src/G03/P022_NameScores.java
@@ -9,26 +9,24 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 import org.junit.Test;
-/**
- * 
- * @author Harold Almon
- *
- */
+
 public class P022_NameScores {
 	public int nameScores(String fileName) throws IOException {
 		FileInputStream fileInputStream = new FileInputStream(fileName);
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
 		String line = null;
+		
 		if((line = bufferedReader.readLine()) == null) {
 			System.out.printf("%s: File read error! Cannot read %s%n", this.getClass().getName(), fileName);
 			System.exit(-1);
 		}
+
 		bufferedReader.close();
 		String[] namesAry;
 		namesAry = line.split(",");
 		Arrays.sort(namesAry);
-		int fileSum = 0;
 
+		int fileSum = 0;
 		for (int nameIndex = 0; nameIndex < namesAry.length; nameIndex++) {
 			fileSum += calculateNameScore(namesAry, nameIndex);
 		}
@@ -41,23 +39,23 @@ public class P022_NameScores {
 
 		byteName = namesAry[nameIndex].toCharArray();
 		nameSum = 0;
-		nameSum = calculateNameSum(nameSum, byteName);
+		nameSum = nameSum(nameSum, byteName);
 		return nameSum * (nameIndex + 1);
 	}
 
-	private int calculateNameSum(int nameSum, char[] byteName) {
+	private int nameSum(int nameSum, char[] byteName) {
 		for (int digitPosition = 1; digitPosition < byteName.length-1; digitPosition++) {
 			nameSum += Character.getNumericValue(byteName[digitPosition]) - 9;
 		}
 		return nameSum;
 	}
 
-	@Test
+	@Test(timeout = 500)
 	public void NameScores() throws IOException {
 		String inputFile = "src/g03/p022_names.txt";
 		int score = nameScores( inputFile );
 
-		System.out.printf("NameScores(%s) = %s%n",inputFile, score);
+		System.out.printf("nameScores(%s) = %s%n",inputFile, score);
 		assertEquals(871198282, score);
 	}
 }

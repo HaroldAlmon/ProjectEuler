ba27a6a608149e9ca4018578e1bf863432f1f788
haroldalmon <haroldemail@gmail.com>
2015-05-30 07:26:54 -0500

    Remove import

--- src/G01/P001_MultiplesOf3And5.java
+++ src/G01/P001_MultiplesOf3And5.java
@@ -1,6 +1,5 @@
 package G01;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import static org.hamcrest.CoreMatchers.*;
--- src/G01/P006_SumSquareDifference.java
+++ src/G01/P006_SumSquareDifference.java
@@ -3,37 +3,41 @@ package G01;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import common.FastTest;
 /** Strategy: Simple mathematics. */
+@Category( FastTest.class )
 public class P006_SumSquareDifference {
-	private long sumSquareDifference(int maxNum) {
-		long result = 0;
-		result = squareSum(maxNum) - sumSquares(maxNum);
+	private long sumSquareDifference( int maxNum ) {
+		long squareDifference = 0;
+		squareDifference = squareSum( maxNum ) - sumSquares( maxNum );
 		
 		
-		return result;
+		return squareDifference;
 	}
 	
-	private long sumSquares(int maxNum) {
+	private long sumSquares( int maxNum ) {
 		long result = 0;
-		for(int num = 1; num <= maxNum; num++) {
+		for( int num = 1; num <= maxNum; num++ ) {
 			result += num*num;
 		}
 		return result;
 	}
 	
-	private long squareSum(int maxNum) {
+	private long squareSum( int maxNum ) {
 		long result = 0;
-		for(int num= 1; num <= maxNum; num++) {
+		for( int num= 1; num <= maxNum; num++ ) {
 			result += num;
 		}
 		return result * result;
 	}
 	
-	@Test(timeout = 500)
+	@Test( timeout = 500 )
 	public void SumSquareDifference() {
-		long sum = sumSquareDifference(100);
-		System.out.printf("sumSquareDifference(%d) = %d%n", 100, sum);
-		assertEquals( "Incorrect sum", 25164150, sum );
+		long sum = sumSquareDifference( 100 );
+		System.out.printf( "sumSquareDifference( %d ) = %d%n", 100, sum );
+		assertEquals(  "Incorrect sum", 25164150, sum  );
 	}
 	
 }

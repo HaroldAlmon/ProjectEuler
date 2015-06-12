aae118287414d7c8e242bdf19159674467606131
haroldalmon <haroldemail@gmail.com>
2015-06-12 04:43:02 -0500

    Make variable private

--- src/G01/P008_LargestProduct.java
+++ src/G01/P008_LargestProduct.java
@@ -44,7 +44,7 @@ public class P008_LargestProduct {
 		return Integer.parseInt( inputString.substring( charPosition, charPosition+1 ) );
 	}
 
-	 static String veryLongNumber = 
+	private static String veryLongNumber = 
 	 "73167176531330624919225119674426574742355349194934"+
 	 "96983520312774506326239578318016984801869478851843"+
 	 "85861560789112949495459501737958331952853208805511"+

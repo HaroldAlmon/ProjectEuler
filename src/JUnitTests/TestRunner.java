79a47299ca9ab10a0a8df3b524d9f963a6dc352f
haroldalmon <haroldemail@gmail.com>
2015-06-17 03:49:17 -0500

    Initial version

---
+++ src/JUnitTests/TestRunner.java
@@ -0,0 +1,13 @@
+package JUnitTests;
+
+import org.junit.runner.JUnitCore;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		JUnitCore.runClasses(P001_MultiplesOf3And5Test.class);
+		JUnitCore.runClasses(P004_PalindromeProductTest.class);
+		JUnitCore.runClasses(P010_SummationOfPrimesTest.class);
+	}
+
+}

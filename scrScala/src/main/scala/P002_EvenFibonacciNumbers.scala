e0aaad9c4b7d9cbeea537acc6ea0a4455fc0e785
haroldalmon <haroldemail@gmail.com>
2015-06-27 19:40:13 -0500

    Initial version of Scala file

---
+++ scrScala/src/main/scala/P002_EvenFibonacciNumbers.scala
@@ -0,0 +1,29 @@
+/**
+ * Created by Harold on 6/27/2015.
+ */
+object P002_EvenFibonacciNumbers extends App {
+  println(EvenFiboNumbersSum(4000000 ))
+
+ // public class P002_EvenFibonacciNumbers {
+    def EvenFiboNumbersSum(upperLimit: Int) = {
+      var EvenFiboNumbersSum = 2
+      var fibo1 = 1
+      var fibo2 = 2
+      var nextFibo = 3
+
+      /* Use a loop since we do not know the term
+       * of the Fibonacci number we are seeking. */
+      while (nextFibo < upperLimit) {
+        var isEven = false
+
+        isEven = (nextFibo % 2 == 0)
+        if( isEven ) {
+          EvenFiboNumbersSum += nextFibo
+        }
+        fibo1 = fibo2
+        fibo2 = nextFibo
+        nextFibo = fibo1 + fibo2
+      }
+      EvenFiboNumbersSum
+    }
+}

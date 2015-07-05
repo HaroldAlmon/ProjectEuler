95bbceaa7a5ac16f6bc93a699ba36f140c4cb46c
haroldalmon <haroldemail@gmail.com>
2015-07-05 06:09:37 -0500

    Add more code

--- src/G03/P023_NonAbundantSums.java
+++ src/G03/P023_NonAbundantSums.java
@@ -14,14 +14,17 @@ import G03.P021_AmicableNumbers.ProperDivisors;
 
 public class P023_NonAbundantSums {
 	ProperDivisors properDivisors = new ProperDivisors();
-	public int nonAbundantSums() {
+	public long nonAbundantSums() {
 		int[] divisors;
-		List<Integer> abundant= new ArrayList<>();
-		Set<Integer> abundantSums= new HashSet<>();
+		long grandTotal = 0;
+		int upperLimit = 100;
 		
-		for (int candidate = 12; candidate < 28123 - 12; candidate ++) {
-			divisors = properDivisors.properDivisors(candidate);
-			if( sum(divisors) > candidate) {
+		List<Integer> abundant = new ArrayList<>();
+		Set<Integer> abundantSums = new HashSet<>();
+		
+		for (int candidate = 12; candidate < upperLimit - 12; candidate ++) {
+			divisors = properDivisors.properDivisors( candidate) ;
+			if( sum( divisors ) > candidate) {
 				abundant.add(candidate);
 			}
 		}
@@ -30,12 +33,27 @@ public class P023_NonAbundantSums {
 		// Add sums to set
 		// enumerate all numbers <= 28123
 		// if not in set add to answerSum
+		
+		for ( int i = 0; i < abundant.size(); i++ ) {
+			for ( int j = 0; j < abundant.size(); j++ )
+				abundantSums.add( abundant.get(i) + abundant.get(j) );
+		}
+		
+		for ( int i = 24; i < upperLimit; i++ ) {
+			if ( abundantSums.contains(1) == false ) {
+				grandTotal += i;
+			}
+		}
 
-		System.out.println("Total no in set = " + abundant.size() );
-		System.out.println(abundant.toString());
-		Collections.reverse(abundant);
-		System.out.println(abundant.toString());
-		return 1;
+		System.out.println( "Total no in set = " + abundant.size() );
+		System.out.println( "Total no in Sums set = " + abundantSums.size() );
+		System.out.println( abundant.toString() );
+		
+		// For debug...
+		Collections.reverse( abundant );
+		System.out.println(abundant.toString() );
+		System.out.println("gTotal = " + grandTotal);
+		return grandTotal;
 	}
 	
 	int sum(int [] array){

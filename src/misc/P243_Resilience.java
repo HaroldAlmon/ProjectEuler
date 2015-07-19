368e77920733064d82907a8b9e38938b0a710ac4
haroldalmon <haroldemail@gmail.com>
2015-07-19 18:27:59 -0500

    This version works with the example problem R(d) < 4/10

--- src/misc/P243_Resilience.java
+++ src/misc/P243_Resilience.java
@@ -1,40 +1,85 @@
 package misc;
-import divisors;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import G03.P021_AmicableNumbers.ProperDivisors;
 
 // Find the smallest denominator d, having a resilience R(d) < 15499/94744
 public class P243_Resilience {
-	function getResilience() begin 
-		int upperLimit = 15499;
-		int candidate = 94744
-		set numeratorDivisors
-		set denomiatorDivisors
-		while true do
-			denomiatorDivisors = calculateDivisors(candidate)
-			
-			for i=2 to candidate
-				numeratorDivisors = calculateDivisors(i)
-				boolean proper = true
+	ProperDivisors properDivisors = new ProperDivisors();
+	public int getResilience() { 
+		int lowerLimit = 4;
+		int uppperLimit = 10;
+		int candidate = 11;
+		int total = 0;
 
-				foreach divisor in numeratorDivisors
-					if denomiatorDivisors contains(divisor)
-						proper = false
-					end if
-				end
+		while ( candidate < 14 ) {
+			int[] divisors;
 
-				if proper = true
-					total += 1
-					
-				// Too many
-				if total >= upperLimit then
-					break;
-			end for
-			
-			
-			if total < upperLimit then
-				break
-			end if
+
+			Set<Integer> denominatorSet;
+			divisors = properDivisors.properDivisors( candidate );
+			System.out.printf("denominator set = %s%n", Arrays.toString(divisors));
+			//denominatorSet = new HashSet<Integer>( Arrays.asList(divisors) );
+			denominatorSet = new HashSet<Integer>();
+			for(int i : divisors) {
+				denominatorSet.add(Integer.valueOf(i));
+			}
+
+			System.out.printf("Candidate denominator = %s%n", candidate);
+			for (int i=2; i < candidate; i++) {
+				System.out.printf("numerator = %s%n",i);
+				divisors = properDivisors.properDivisors(i);
+				System.out.printf("numerator set = %s%n", Arrays.toString(divisors));
+				boolean proper = true;
+
+				if ( denominatorSet.contains( Integer.valueOf( i ) ) == true) {
+					proper = false;
+				} else {
+					for( Integer divisor : divisors) {
 	
-			candidate += 1
-		end while
-	end function
+						if (denominatorSet.contains(divisor) && divisor != 1) {
+							proper = false;
+						}
+					}
+				}
+
+				if ( proper == true )
+					total += 1;
+				
+				System.out.printf("total = %s%n",total);
+				// Too many
+				/*if (total >= lowerLimit) {
+					System.out.printf("Too many break %s%n");
+					break;
+				}*/
+				
+				// Check if ratio is too big...
+				if (( (float )total/(candidate-1)) >= ((float)lowerLimit/uppperLimit)) {
+					break;
+				}
+				//break;
+			}
+
+			if ((float)total/(candidate-1) < (float)lowerLimit/uppperLimit) {
+				break;
+			}
+
+			candidate += 1;
+			total = 0;
+		}
+		return candidate;
+	}
+	
+	@Test
+	public void test1() {
+		int result;
+		result = getResilience();
+		System.out.printf("Resilience = %s\n", result);
+	}
 }
+
+

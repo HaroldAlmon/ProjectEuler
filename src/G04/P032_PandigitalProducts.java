37a7f9d2f25751d531cc1cc40b3ad6f895f445f0
haroldalmon <haroldemail@gmail.com>
2015-08-20 22:06:56 -0500

    Initial version

---
+++ src/G04/P032_PandigitalProducts.java
@@ -0,0 +1,15 @@
+package G04;
+
+import org.junit.Test;
+
+public class P032_PandigitalProducts {
+	public int product() {
+		int result = 0;
+		return result;
+	}
+
+	@Test
+	public void test1() {
+		
+	}
+}

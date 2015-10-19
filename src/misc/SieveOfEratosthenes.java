926d764b0a6736b9297ea853484f86607616ea9a
haroldalmon <haroldemail@gmail.com>
2015-10-19 17:59:58 -0500

    Initial version.

---
+++ src/misc/SieveOfEratosthenes.java
@@ -0,0 +1,35 @@
+package misc;
+
+public class SieveOfEratosthenes {
+	private boolean[] primes;
+
+	public int generatePrimes(int n) {
+		primes = new boolean[n + 1];
+
+		for (int position = 0; position <= n; position += 1) {
+			primes[position] = true;
+		}
+		
+		int upperBound = (int) Math.floor(Math.sqrt( n ));
+		
+		for (int primeNumber = 2; primeNumber <= upperBound; primeNumber += 1) {
+			if( primes[primeNumber] == true) {
+				for( int notPrime = primeNumber * primeNumber; notPrime <= n; notPrime += primeNumber)
+					primes[notPrime] = false;
+			}
+		}
+		
+		int count = 0;
+		for( int position = 2; position <= n; position += 1) {
+			if(primes[position] == true) {
+				count += 1;
+			}
+		}
+		System.out.printf("Generated %d primes%n", count);
+		return count;
+	}
+	
+	public boolean isPrime(int n) {
+		return primes[n];
+	}
+}

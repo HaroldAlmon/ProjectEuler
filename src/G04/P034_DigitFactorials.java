312997e95125e6548f87b25fa3ae8c20043f23ea
haroldalmon <haroldemail@gmail.com>
2015-10-24 17:39:43 -0500

    Refactor with new functions.

--- src/G04/P034_DigitFactorials.java
+++ src/G04/P034_DigitFactorials.java
@@ -8,40 +8,50 @@ import java.util.List;
 import org.junit.Test;
 
 public class P034_DigitFactorials {
-	public int sum() {
-		int sum = 0;
-		List<Integer> factorial = new ArrayList<>(100);
-		for(int i = 0; i <= 9; i += 1) {
-			factorial.add(Integer.valueOf(factorial(i)));
-		}
+	public int digitFactorials() {
+		int totalFactorialSum = 0;
+		List<Integer> factorial = new ArrayList<>( 100 );
+		memoizeFactorials(factorial);
 		
-		System.out.printf("x = %d%n", factorial(4));
-		
-		for(int number = 10; number < 1000000; number +=1) {
+		for( int candidate = 10; candidate < 1_000_000; candidate +=1 ) {
 			int factorialSum = 0;
-			String s = String.valueOf(number);
-			for(int i = 0; i < s.length(); i += 1) {
-				factorialSum += factorial.get( (int) s.charAt(i) - 0x30 );
-			}
-			if(number == factorialSum) {
-				System.out.printf("Match!!! sum %d! = %d%n", number, factorialSum);
-				sum += number;
+			String digits = String.valueOf( candidate );
+			
+			factorialSum = factorialSum( factorial, factorialSum, digits );
+			
+			if( candidate == factorialSum ) {
+				System.out.printf( "factorialSum( %d ) = %d%n", candidate, factorialSum );
+				totalFactorialSum += candidate;
 			}
 		}
-		return sum;
+		return totalFactorialSum;
+	}
+
+	private int factorialSum(List<Integer> factorial, int factorialSum,
+			String digits) {
+		for( int position = 0; position < digits.length(); position += 1 ) {
+			factorialSum += factorial.get( (int) digits.charAt(position) - 0x30 );
+		}
+		return factorialSum;
+	}
+
+	private void memoizeFactorials(List<Integer> factorial) {
+		for( int i = 0; i <= 9; i += 1 ) {
+			factorial.add( Integer.valueOf( factorial( i ) ) );
+		}
 	}
 	
-	private int factorial(int n) {
-		if(n == 1 || n == 0) {
+	private int factorial( int n ) {
+		if( n == 1 || n == 0 ) {
 			return 1;
 		}
-		return n * factorial( n - 1);
+		return n * factorial( n - 1 );
 	}
 	
 	@Test
 	public void runTest() {
-		int sum = sum();
-		System.out.printf("sum = %d%n", sum);
-		assertEquals(sum, 40730);
+		int sum = digitFactorials();
+		System.out.printf( "%nDigital Factorial Sum = %d%n", sum );
+		assertEquals( sum, 40730 );
 	}
 }

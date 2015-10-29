96c2188a62d5c7ec1dde66298ef762adbe8aaf8b
haroldalmon <haroldemail@gmail.com>
2015-10-29 13:17:09 -0500

    Cleanup code

--- src/G05/P041_PandigitalPrime.java
+++ src/G05/P041_PandigitalPrime.java
@@ -1,26 +1,21 @@
 package G05;
 
-
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
-
 import G03.P024_LexicographicPermutationsDesc;
-//import misc.SieveOfEratosthenesBitSet;;
+import misc.SieveOfEratosthenes;
 
 public class P041_PandigitalPrime {
 	public long getNumber() {
-		long result = -1;
-		//SieveOfEratosthenesBitSet sieve = new SieveOfEratosthenesBitSet();
-		//sieve.generatePrimes(Integer.MAX_VALUE -1);
+		int result = -1;
+		SieveOfEratosthenes sieve = new SieveOfEratosthenes();
+		sieve.generatePrimes(10_000_000);
 		P024_LexicographicPermutationsDesc iter = new P024_LexicographicPermutationsDesc();
-		//int[] number = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-		int[] number = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-		//int[] number = { 3, 1, 0 };
+		int[] number = { 7, 6, 5, 4, 3, 2, 1 };
 		
 		do {
-			result = charArrayToLong( number );
-			if ( isPrime( result ) )
+			result = charArrayToInt( number );
+			if ( sieve.isPrime( result ) )
 				break;
 		} while ( iter.nextPermutation(number) );
 		return result;
@@ -50,7 +45,7 @@ public class P041_PandigitalPrime {
 	    return result;
 	}
 	
-	private boolean isPrime( long number ) {
+	private boolean isPrime( int number ) {
 		return ( (( number - 1 ) % 6) == 0 ) || ( ( ( number + 1 ) % 6 ) == 0 );
 	}
 
@@ -59,29 +54,8 @@ public class P041_PandigitalPrime {
 		long number;
 		P041_PandigitalPrime pandigitalPrime = new P041_PandigitalPrime();
 		number = pandigitalPrime.getNumber();
-		System.out.printf( "number = %010d%n", number );
-		assertEquals( number, 1286608609 );
-	}
-	
-	//@Test
-	public void test2() {
-		//int[] number = { 4, 2, 1, 0 };
-		int[] number = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-		P024_LexicographicPermutationsDesc iter = new P024_LexicographicPermutationsDesc();
-		
-		long result = charArrayToInt(number);
-		do {
-			result = charArrayToInt( number );
-			System.out.printf("%010d%n", result);
-			//if ( isPrime( result ) )
-				//break;
-		} while ( iter.nextPermutation(number) );
-	}
-	@Test
-	public void TestPrimes() {
-		for (int i = 4; i <= 100; i ++)
-			if (isPrime(i)) 
-				System.out.printf("%d is prime%n", i);
+		System.out.printf( "number = %d%n", number );
+		assertEquals( 7652413, number );
 	}
 }
 

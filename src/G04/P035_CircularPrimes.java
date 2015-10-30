7043cae8fa898eecd605a3bad627af1f0793464a
haroldalmon <haroldemail@gmail.com>
2015-10-30 16:33:25 -0500

    Refactor into functions

--- src/G04/P035_CircularPrimes.java
+++ src/G04/P035_CircularPrimes.java
@@ -6,36 +6,44 @@ import org.junit.Test;
 
 public class P035_CircularPrimes {
 	SieveOfEratosthenes sieve = new SieveOfEratosthenes();
-	public int sum( int upperLimit ) {
-		int sum = 0;		
-		int primeCount = sieve.generatePrimes(upperLimit);
-		System.out.printf("Prime Count = %d%n", primeCount);
+	public int countPrimes( int upperLimit ) {
+		int circularPrimeCount = 0;		
+		sieve.generatePrimes(upperLimit);
 
 		for(int number = 2; number < upperLimit; number += 1) {
 			String candidatePrime = Integer.toString(number);
 			
-			int numLength = candidatePrime.length();
-			
-			int rotations;
-			for(rotations = 1; rotations <= numLength; rotations += 1) {
-				if(sieve.isPrime(Integer.parseInt(candidatePrime)))
-					candidatePrime = candidatePrime.substring(1) + candidatePrime.substring(0,1);
-				else
-					break;
-			}
-		
-			if( rotations == numLength + 1) {
-				sum += 1;
-				System.out.printf("Circular Prime: %d%n", number);
+			if ( isCircularPrime(candidatePrime) ) {
+				circularPrimeCount += 1;
 			}
 		}
-		return sum;
+		return circularPrimeCount;
+	}
+	
+	boolean isCircularPrime(String candidatePrime) {
+		int rotations;
+		int numLength = candidatePrime.length();
+		for (rotations = 1; rotations <= numLength; rotations += 1) {
+			if (sieve.isPrime(Integer.parseInt(candidatePrime)))
+				candidatePrime = rotateString(candidatePrime);
+			else
+				break;
+		}
+		return isRotationComplete(rotations, numLength);
+	}
+
+	private boolean isRotationComplete(int rotations, int numLength) {
+		return rotations == numLength + 1;
+	}
+
+	private String rotateString(String candidatePrime) {
+		return candidatePrime.substring(1) + candidatePrime.substring(0,1);
 	}
 	
 	@Test 
 	public void runTest() {
-		int upperLimit = 100;
+		int upperLimit = 1_000_000;
 
-		System.out.printf("Circular Prime Count = %d%n", sum(upperLimit));
+		System.out.printf("Circular Prime Count = %d%n", countPrimes(upperLimit));
 	}
 }

2a231d87c1bee637fb4a7554a23f060a420b9591
haroldalmon <haroldemail@gmail.com>
2015-11-11 08:50:25 -0600

    Initial version.

---
+++ src/G05/P046_GoldbachsOtherConjecture.java
@@ -0,0 +1,19 @@
+package G05;
+
+import org.junit.Test;
+
+public class P046_GoldbachsOtherConjecture {
+/*
+generate 100 prime numbers
+create a sieve that adds the primes to 2(1..100)
+check the sieve for the first number that is not generated
+ */
+	public int smallestNumber(int upperPrime, int upperNumber) {
+		return 0;
+	}
+	
+	@Test
+	public void GoldbachsOtherConjecture() {
+		smallestNumber(100, 100);
+	}
+}

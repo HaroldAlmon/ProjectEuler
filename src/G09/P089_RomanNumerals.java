efdca77fa984af76ad1805965164b246477a64ac
haroldalmon <haroldemail@gmail.com>
2015-11-14 07:20:05 -0600

    Removed debug code.

--- src/G09/P089_RomanNumerals.java
+++ src/G09/P089_RomanNumerals.java
@@ -2,11 +2,8 @@ package G09;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
 import org.junit.Test;
 
 public class P089_RomanNumerals {
@@ -27,61 +24,50 @@ public class P089_RomanNumerals {
 		return total;
 	}
 	private int savings(String number) {
-		int result = 0;
+		int savedCharacters = 0;
 		
-		result = minimise9and4(number, result);
-		result = minimise90and40(number, result);
-		result = minimise900and400(number, result);
+		savedCharacters = minimise9and4(number, savedCharacters);
+		savedCharacters = minimise90and40(number, savedCharacters);
+		savedCharacters = minimise900and400(number, savedCharacters);
 		
-		return result;
+		return savedCharacters;
 	}
-	private int minimise900and400(String number, int result) {
+	private int minimise900and400(String number, int savedCharacters) {
 		if ( number.contains("DCCCC") ) {
-			result += 3;
+			savedCharacters += 3;
 
 		} else if(number.contains("CCCC"))  {
-			result += 2;			
+			savedCharacters += 2;			
 		}
-		return result;
+		return savedCharacters;
 	}
-	private int minimise90and40(String number, int result) {
+	private int minimise90and40(String number, int savedCharacters) {
 		if ( number.contains("LXXXX") ) {
-			System.out.println(number);
-			result += 3;
+			savedCharacters += 3;
 			
 		} else if ( number.contains("XXXX") ) {
-			System.out.println(number);
-			result += 2;
+			savedCharacters += 2;
 		}
-		return result;
+		return savedCharacters;
 	}
-	private int minimise9and4(String number, int result) {
+	private int minimise9and4(String number, int savedCharacters) {
 		if ( number.contains("VIIII") ) {
-			result += 3;
+			savedCharacters += 3;
 
 		} else if (number.contains("IIII")) {
-			result += 2; 
+			savedCharacters += 2; 
 		}
-		return result;
+		return savedCharacters;
 	}
-/*
- * IIII = IV saves 2
- * IIIII = V saves 4
- * IIIIII = VI saves 4
- * VIIII = IX saves 3
- * XXXX = XL saves 2
- * LXXXX = XC saves 2
- * XXXX = XL saves 2
- */
+
 	@Test
 	public void RomanNumerals() {
 		String inputFile = "src/g09/P089_roman.txt";
-		int total = 0;
+		int charactersSaved = 0;
 		try {
-			total = characterSaved(inputFile);
-			System.out.println(total);
+			charactersSaved = characterSaved(inputFile);
+			System.out.printf("Characters saved %d%n", charactersSaved);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}

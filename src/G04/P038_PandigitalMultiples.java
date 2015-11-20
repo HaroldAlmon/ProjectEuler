8469af59ce9e36e472239f0fb468c94aeab7d3e5
haroldalmon <haroldemail@gmail.com>
2015-11-20 07:06:42 -0600

    Refactor into new functions.

--- src/G04/P038_PandigitalMultiples.java
+++ src/G04/P038_PandigitalMultiples.java
@@ -13,32 +13,40 @@ public class P038_PandigitalMultiples {
 		//maximum = checkRange(maximum, 9, 9); // NOT NEEDED
 		//maximum = checkRange(maximum, 91, 98); NOT POSSIBLE
 		//maximum = checkRange(maximum, 912, 987); NOT POSSIBLE
-		maximum = checkRange(maximum, 9134, 9876);
+		maximum = enumerateBase(maximum, 9134, 9876);
 		//maximum = checkRange(maximum, 1, 9_999);
 		return maximum;
 	}
 	
-	private int checkRange(int maximum, int lower, int upper) {
+	private int enumerateBase(int maximum, int lower, int upper) {
 		for (int base = lower; base <= upper; base += 1) {
 			String candidate = "";
-			for (int multiplier = 1; multiplier <= 9 && candidate.length() <= 9; multiplier += 1) {
-				candidate += Integer.toString(base * multiplier);
-				if (candidate.length() == 9) {
-					if ( isPandigital(candidate) ) {
-						System.out.printf("base = %d, found %s%n", base, candidate);
-						maximum = Math.max(maximum, Integer.parseInt(candidate));
-					}
-					break;
+			maximum = enumerateMutiplier(candidate, base, maximum);
+		}
+		return maximum;
+	}
+
+	private int enumerateMutiplier(String candidate, int base, int maximum) {
+		for (int multiplier = 1; multiplier <= 9 && candidate.length() <= 9; multiplier += 1) {
+			candidate += Integer.toString(base * multiplier);
+			if (candidate.length() == 9) {
+				if ( isPandigital(candidate) ) {
+					//System.out.printf("base = %d, found %s%n", base, candidate);
+					maximum = Math.max(maximum, Integer.parseInt(candidate));
 				}
+				break;
 			}
 		}
 		return maximum;
 	}
 	 
 	private boolean isPandigital(String digits) {
+		if ( digits.contains("0") )
+			return false;
+
 		Set<Character> dupeFilter = new HashSet<>();
 		for (int i = 0; i < digits.length(); i += 1) {
-			if ( dupeFilter.add(digits.charAt(i)) == false || digits.charAt(i) == '0'  ) {
+			if ( dupeFilter.add(digits.charAt(i)) == false ) {
 				return false;
 			}
 		}
@@ -49,30 +57,7 @@ public class P038_PandigitalMultiples {
 	public void PandigitalMultiples() {
 		int pandigitalMaximum = 0;
 		pandigitalMaximum = pandigitalMaximum();
-		System.out.printf("max = %d%n", pandigitalMaximum);
+		System.out.printf("Maximum pandigital number is %d%n", pandigitalMaximum);
 		assertEquals(pandigitalMaximum, 932718654);
 	}
 }
-
-/*
- * maximum = checkRange(maximum, 91, 98);
- * maximum = checkRange(maximum, 918, 987);
- * return maximum;
- * 
- *  checkRange(maximum, lower, upper) {
- *  	for(int base = lower; base <= upper; base += 1) {
- *  		if ( areDigitsUnique(base) == false )
- *  			continue;
- *  		String candidate = "";
- *			for(int multiplier = 1; mutiplier <= 9; multiplier += 1) {
- *				candidate += Integer.toString( base * mutiplier );
- *				if (candidate.length() == 9)
- *					break 				
- *			}
- *			maximum = Math.max(	maximum, Integer.parseInt(candidate) ); 	  
- *  	}
- *  
- *  areDigitsUnique(int number) {
- *  	
- *  }
- * */

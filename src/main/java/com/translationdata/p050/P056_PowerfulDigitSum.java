b9f801cf67e09cca143a1573830ebaec50b30eae
Harold Almon <haroldemail@gmail.com>
2017-02-03 17:55:35 +0800

    Initial version

---
+++ src/main/java/com/translationdata/p050/P056_PowerfulDigitSum.java
@@ -0,0 +1,45 @@
+package com.translationdata.p050;
+
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class P056_PowerfulDigitSum {
+  int powerfulDigitSum() {
+    System.out.println("P056_PowerfulDigitSum");
+    
+    List<Integer> powers = new ArrayList<Integer>();
+    for(int base = 1; base <= 99; base += 1) {
+    	for(int exponent = 1; exponent <= 99; exponent += 1) {
+    		BigInteger bigBase = new BigInteger(Integer.toString(base));
+    		powers.add(stringSum( bigBase.pow(exponent).toString() ) );
+    	}
+    }
+    
+    return Collections.max(powers).intValue();
+  }
+
+  int stringSum(String numberString){
+    return stringSumImpl(numberString, 0);
+  }
+  
+  int stringSumImpl(String numberString, int sum) {
+      if ( numberString.isEmpty() )
+        return sum;
+      else
+        return stringSumImpl(numberString.substring(1), sum + numberString.charAt(0) - '0');
+    }
+  
+  @Test 
+  public void testP056_PowerfulDigitSum() {
+	  P056_PowerfulDigitSum pds = new P056_PowerfulDigitSum();
+	  int result = pds.powerfulDigitSum();
+	  assertThat("Incorrect sum.", result, is(972));
+	  System.out.println( "Maximum digital sum = " + result );
+  }
+}

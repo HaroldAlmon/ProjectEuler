93d535e2972cd88bff810e86770a5779eb0a444d
Harold Almon <haroldemail@gmail.com>
2017-02-03 18:47:16 +0800

    Add range

--- src/main/java/com/translationdata/p050/P056_PowerfulDigitSum.java
+++ src/main/java/com/translationdata/p050/P056_PowerfulDigitSum.java
@@ -1,24 +1,33 @@
 package com.translationdata.p050;
 
 import static org.hamcrest.CoreMatchers.*;
-import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
 
 public class P056_PowerfulDigitSum {
   int powerfulDigitSum() {
     System.out.println("P056_PowerfulDigitSum");
     
+    final List<Integer> range1to99 = IntStream
+    		.rangeClosed(1, 99)
+    	    .boxed()
+    	    .collect(Collectors.toList());
+    
     List<Integer> powers = new ArrayList<Integer>();
-    for(int base = 1; base <= 99; base += 1) {
-    	for(int exponent = 1; exponent <= 99; exponent += 1) {
+    
+    for(int base : range1to99) {
+        for(int exponent : range1to99) {
     		BigInteger bigBase = new BigInteger(Integer.toString(base));
     		powers.add(stringSum( bigBase.pow(exponent).toString() ) );
-    	}
+        }
     }
     
     return Collections.max(powers).intValue();
@@ -37,8 +46,8 @@ public class P056_PowerfulDigitSum {
   
   @Test 
   public void testP056_PowerfulDigitSum() {
-	  P056_PowerfulDigitSum pds = new P056_PowerfulDigitSum();
-	  int result = pds.powerfulDigitSum();
+	  final P056_PowerfulDigitSum pds = new P056_PowerfulDigitSum();
+	  final int result = pds.powerfulDigitSum();
 	  assertThat("Incorrect sum.", result, is(972));
 	  System.out.println( "Maximum digital sum = " + result );
   }
